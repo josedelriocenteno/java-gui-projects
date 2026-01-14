599f22b5176a149e119d258f0f1b811d72a9a1bd
josemigueldelrio <josemiguel.delrio.centeno@gregoriofer.com>
2026-01-14 13:03:37 +0100

    Inicialización de proyectos Java GUI con JFrame y JPanel

---
+++ src/main/java/JavaGuiProjects.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author delcenjo
+ */
+public class JavaGuiProjects {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
